e207a31ab7d50b77c389d16c18ce06f84c9fc0ae
XF <2581011320@qq.com>
2022-05-05 22:48:03 +0800

    2

---
+++ 2/stu.sql
@@ -0,0 +1,70 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : XFdMysql
+ Source Server Type    : MySQL
+ Source Server Version : 80027
+ Source Host           : localhost:3306
+ Source Schema         : stu
+
+ Target Server Type    : MySQL
+ Target Server Version : 80027
+ File Encoding         : 65001
+
+ Date: 15/03/2022 16:39:33
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for course
+-- ----------------------------
+
+-- ----------------------------
+-- Records of course
+-- ----------------------------
+
+
+-- ----------------------------
+-- Table structure for department
+-- ----------------------------
+
+-- ----------------------------
+-- Records of department
+-- ----------------------------
+
+INSERT INTO `department` VALUES (NULL, NULL, NULL);
+
+-- ----------------------------
+-- Table structure for dorm
+-- ----------------------------
+
+-- ----------------------------
+-- Records of dorm
+-- ----------------------------
+
+INSERT INTO `dorm` VALUES (NULL, NULL);
+
+-- ----------------------------
+-- Table structure for grade
+-- ----------------------------
+
+
+-- ----------------------------
+-- Records of grade
+-- ----------------------------
+
+INSERT INTO `grade` VALUES (NULL, NULL, NULL);
+
+-- ----------------------------
+-- Table structure for student
+-- ----------------------------
+
+
+-- ----------------------------
+-- Records of student
+-- ----------------------------
+
+
+
